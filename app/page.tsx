d66996b2160a5e15c3b4f25825daf6745e4ccf22
NahomKasa <nahomkasa345@gmail.com>
2025-07-12 04:56:03 +0300

    feat: Calculate sleep duration and get feedback

--- app/page.tsx
+++ app/page.tsx
@@ -11,26 +11,39 @@ function page() {
       alert('Please enter both sleep and wake times.')
       return
     }
-
     const sleepTimeParts = sleepingtime.split(':').map(Number)
     const wakeTimeParts = wakingtime.split(':').map(Number)
 
     const sleepDate = new Date()
     sleepDate.setHours(sleepTimeParts[0], sleepTimeParts[1], 0)
-
     const wakeDate = new Date()
     wakeDate.setHours(wakeTimeParts[0], wakeTimeParts[1], 0)
-
-    if (wakeDate <= sleepDate) {
-      wakeDate.setDate(wakeDate.getDate() + 1)
+    if (wakeDate < sleepDate) {
+      wakeDate.setDate(wakeDate.getDate() + 1) // Adjust for next day if wake time is earlier than sleep time
     }
-
-    const duration = (wakeDate.getTime() - sleepDate.getTime()) / (1000 * 60 * 60)
-    
-    alert(`You slept for ${duration} hours.`)
+    const sleepTimeInHours = (wakeDate.getTime() - sleepDate.getTime()) / (1000 * 60 * 60) // Convert milliseconds to hours
+    const Hour = Math.floor(sleepTimeInHours)
+    const Minutes = Math.floor((sleepTimeInHours - Hour) * 60)
+    if (Hour > 0 && Minutes == 0) {
+      const sleepDuration = `${Hour} hours`
+      alert(`You slept for ${sleepDuration} hours.`)
+    } else {
+      const sleepDuration = `${Hour} hours and ${Minutes} minutes`
+      alert(`You slept for ${sleepDuration}.`)
+    }
+    getSleepingFeedback()
   }
 
-
+   const getSleepingFeedback = () => {
+      const persons_feedback = prompt("Please provide your feedback on your sleep quality 1-10:")
+      const comment = confirm("so you want provide any additional comments: yes or no")
+      if (comment) {
+        const additionalComment = prompt("Please provide your additional comments:")
+        alert(`Thank you for your feedback! You rated your sleep quality as ${persons_feedback} and provided the comment: ${additionalComment}`)
+      } else {
+        alert(`Thank you for your feedback! You rated your sleep quality as ${persons_feedback}`)
+      }
+   }
   return (
     <div>
         <label>
@@ -52,7 +65,7 @@ function page() {
         </label>
         <br />
         <button onClick={handleButtonClick}>
-          Calculate Sleep Duration
+         Sleep Duration
         </button>
     </div>
   )
